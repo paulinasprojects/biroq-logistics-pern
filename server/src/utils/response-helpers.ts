8276dd8acb0650218ef134fc521665ee24b8de43
paulinasprojects <virtualpaula93@gmail.com>
2026-05-01 11:18:05 +0300

    added types and util functions

---
+++ server/src/utils/response-helpers.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from "express";
+import { ApiResponse } from "../types";
+
+export const sendSucess = <T>(
+  res: Response,
+  data: T,
+  message: string = "success",
+  statusCode: number = 200
+) => {
+  
+  const response: ApiResponse<T> = {
+    success: true,
+    data,
+    message,  
+  };
+  
+  res.status(statusCode).json(response);
+};
+
+export const asyncHandler= (fn: Function) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+};
